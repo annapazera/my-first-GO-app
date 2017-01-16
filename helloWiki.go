c4c3abbcc4fe2aaef62fb02e09453f8aafa1190c
Anna Pazera <anna.pazera@rndity.com>
2017-01-16 23:03:52 +0100

    helloWIKI  with  templates exercise of redirecting and displaying the data

---
+++ helloWiki.go
@@ -0,0 +1,33 @@
+package main
+import (
+	"html/template"
+	"io/ioutil"
+	"net/http"
+)
+type Page struct {
+	Title string
+	Body  []byte
+}
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	t, _ := template.ParseFiles("edit.html")
+	t.Execute(w, p)
+}
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	t, _ := template.ParseFiles("view.html")
+	t.Execute(w, p)
+}
